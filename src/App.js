68f4a860c8b89dacd54f6a9542ad88f66ab913c4
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-28 09:23:57 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Router, Route } from "react-router-dom";
+import SubscriptionCreation from "./pages/SubscriptionCreation";
+import SubscriptionList from "./pages/SubscriptionList";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Router>
+          <Route path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route path="/Subscription-creation" component={SubscriptionCreation}/>
+          <Route path="/Subscription-list" component={SubscriptionList} />
+        </Router>
+      </BrowserRouter>
     </div>
   );
 }
