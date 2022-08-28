56617401b2f1bec06fa9184aa7513b8129e2cfdd
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-28 09:14:20 +0530

    pages initialize

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Nav from './components/Nav';
+import Navbar from './components/Navbar';
 
 import '@rainbow-me/rainbowkit/styles.css';
 import {
@@ -43,7 +43,7 @@ root.render(
   <React.StrictMode>
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <Nav/>
+        <Navbar/>
         <App />
       </RainbowKitProvider>
     </WagmiConfig>
---
+++ src/pages/SubscriptionCreation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubscriptionCreation = () => {
+  return (
+    <div>SubscriptionCreation</div>
+  )
+}
+
+export default SubscriptionCreation
